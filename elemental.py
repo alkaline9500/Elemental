21896ebc7ca7c5fe2ccb75ae763dfa5a0a02dd98
Nic Manoogian <zimmoz3@verizon.net>
2014-11-29 12:18:49 -0500

    Changes to a backtracking approach.

--- elemental.py
+++ elemental.py
@@ -7,21 +7,46 @@ __author__ = "Nic Manoogian"
 
 import argparse
 
-class Node:
+class Configuration:
     """
-    A generic linked-list node
+    A word configuration
     """
-    def __init__(self, data)
+    symbols = []
+    target = ""
+    def __init__(self, word):
         """
-        Creates an empty node with data
+        Creates a configuration
         """
-        self.data = data
-        self.nextNode = None
-    def setNext(self, nextNode)
+        self.word = word
+    def successors(self):
         """
-        Sets the next node
+        Generates a list of successors from the current configuration
         """
-        self.nextNode = nextNode
+        successor_list = []
+        for s in symbols:
+            if Configuration.target.startswith(s.lower(), len(self.word)):
+                successor_list.append(Configuration(self.word + s.lower()))
+        return successor_list
+    def is_goal(self):
+        """
+        Returns true if the current configuration is the target
+        """
+        return Configuration.target == self.word
+
+def bfs(word):
+    """
+    Returns true if the word can be represented as a
+    collection of element symbols
+    """
+    queue = []
+    Configuration.target = word
+    queue.append(Configuration(""))
+    while len(queue) > 0:
+        current_config = queue.pop(0)
+        if current_config.is_goal():
+            return True
+        queue.extend(current_config.successors())
+    return False
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Find words that can be spelled using the symbols of elements.")
@@ -29,23 +54,13 @@ if __name__ == "__main__":
     parser.add_argument("symbol_file", help="the filename of the element symbols")
     args = parser.parse_args()
     
-    # Load element symbols into the tree
-    symbols = []
-    for s in open(args.symbol_file):
-        symbol = list(s.strip().lower())
-        rootNode = None
-        currentNode = None
-        for c in symbol:
-            if rootNode == None:
-                rootNode = Node(c)
-                currentNode = rootNode
-            else:
-                
-                
+    # Load element symbols 
+    symbols = [s.strip() for s in open(args.symbol_file)]
+    Configuration.symbols = symbols
 
-
+    # Solve
     for w in open(args.word_list):
-        word = list(w.strip())
-        for c in word:
-            print(c)
+        word = w.strip().lower()
+        if bfs(word):
+            print(word)
 
